e0bfe6c229bb8d6e7146f55da74659d25602d6d1
Akhdanre <danakhdan12@gmail.com>
2024-05-03 18:36:50 +0700

    fix : adjust minor part

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', indexRouter);
+app.use('/', indexRouter);
 app.use('/api/image', imageRoutes);
 
 // catch 404 and forward to error handler
